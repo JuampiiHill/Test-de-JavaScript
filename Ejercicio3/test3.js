29057f0c98ac1951b76670c90fcf9ee1117dd77b
Juan Pablo Hillcoat <juanpablohillcoat@gmail.com>
2022-10-23 20:24:09 -0300

    Se realiza el ejercicio 3, ademas se ingresa una linea donde te lleva al codigo en github

--- Ejercicio3/test3.js
+++ Ejercicio3/test3.js
@@ -1,1 +1,11 @@
-console.log("Hola");
+const tipoDeSuscripcion = "Basic";
+
+if (tipoDeSuscripcion === "Free"){
+    console.log("Solo puedes tomar los cursos gratis");
+} else if (tipoDeSuscripcion === "Basic"){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+} else if (tipoDeSuscripcion === "Expert"){
+    console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+} else if (tipoDeSuscripcion === "ExpertPlus"){
+    console.log("Tu y alguien mas pueden tomar todos los cursos de Platzi durante un año");
+}
