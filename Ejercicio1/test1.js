b41240c91cd776bbd16b30ab5c489e0c7b3a32f5
Juan Pablo Hillcoat <juanpablohillcoat@gmail.com>
2022-10-23 19:59:17 -0300

    Inicio de repositorio. 2 Carpetas con 2 ejercicios realizados del curso practico de JavaScript

---
+++ Ejercicio1/test1.js
@@ -0,0 +1,11 @@
+const name = "Juan Pablo";
+const surname = "Hillcoat";
+const user = "JuampiiHill";
+let age = 27;
+let email = "juanpablohillcoat@gmail.com"
+const adult = true;
+let money = 275000;
+let debts = 198000
+
+console.log("Mi nombre completo es: " + name, surname);
+console.log("Mi dinero real es: " + (money - debts));
---
+++ Ejercicio2/test2.js
@@ -0,0 +1,5 @@
+function presentacion(name, lastname, nickname)
+{
+    const completeName = name + " " + lastname;
+    console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + "." )
+}
