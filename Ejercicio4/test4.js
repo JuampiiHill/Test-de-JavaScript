e614a8c0dee762390d9f73eea01c02fb7139d821
Juan Pablo Hillcoat <juanpablohillcoat@gmail.com>
2022-10-23 21:43:23 -0300

    Se realiza el ejercicio 4

--- Ejercicio4/test4.js
+++ Ejercicio4/test4.js
@@ -1,1 +1,12 @@
-console.log("Hola");
+let i = 0;
+while(i < 5){
+    console.log("El valor de i es: " + i);
+    i++;
+}
+
+let b = 10;
+
+while(b >=2){
+    console.log("El valor de b es: " + b);
+    b--;
+}
